e0eaface00c70bcbe79639fac8686d2617d46e73
Arya <aryadewan123@gmail.com>
2021-05-22 12:18:18 +0530

    added Test3 class and modify Test1

--- src/com/demo/Test1.java
+++ src/com/demo/Test1.java
@@ -3,5 +3,6 @@ package com.demo;
 public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("this is from test1...");
+		System.out.println("ashok modified this class....");
 	}
 }
---
+++ src/com/demo/Test3.java
@@ -0,0 +1,9 @@
+package com.demo;
+
+public class Test3 {
+	
+	public static void main(String[] args) {
+		System.out.println("this is from test 3..");
+	}
+
+}
