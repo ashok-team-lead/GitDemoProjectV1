2eedf21fbae68372526e3b34c43207fb422a926e
Arya <aryadewan123@gmail.com>
2021-05-22 13:59:49 +0530

    added Test5 and Test6

---
+++ src/com/demo/Test5.java
@@ -0,0 +1,8 @@
+package com.demo;
+
+public class Test5 {
+	
+	public static void main(String[] args) {
+		System.out.println("this is from test5...");
+	}
+}
---
+++ src/com/demo/Test6.java
@@ -0,0 +1,8 @@
+package com.demo;
+
+public class Test6 {
+	
+	public static void main(String[] args) {
+		System.out.println("this is from test6..");
+	}
+}
