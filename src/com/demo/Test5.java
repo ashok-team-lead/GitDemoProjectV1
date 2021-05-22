425115141f3fe038788fe5ec944217be77aa5c67
Arya <aryadewan123@gmail.com>
2021-05-22 14:32:29 +0530

    Test5 and Test6 brought from master

--- src/com/demo/Test5.java
+++
@@ -1,8 +0,0 @@
-package com.demo;
-
-public class Test5 {
-	
-	public static void main(String[] args) {
-		System.out.println("this is from test5...");
-	}
-}
--- src/com/demo/Test6.java
+++
@@ -1,8 +0,0 @@
-package com.demo;
-
-public class Test6 {
-	
-	public static void main(String[] args) {
-		System.out.println("this is from test6..");
-	}
-}
